460b3dd3c560762151c531a352e78fbb4f7c956a
Arnas Kromelis <arnas.kromelis@gmail.com>
2019-12-04 14:24:40 +0100

    Day 1 - Part 1 & 2

--- src/days/1/index.js
+++ src/days/1/index.js
@@ -1,9 +1,15 @@
 import input from "./input";
+import { sum } from "../../helpers";
 
-const data = input.split("\n");
+const data = input.split("\n").map(x => parseInt(x, 10));
 
-const run = () => {
-  console.log(data.length);
-};
+const calc = x => Math.floor(x / 3) - 2;
+const recursiveCalc = x =>
+  x === 0 ? 0 : calc(x) <= 0 ? x : x + recursiveCalc(calc(x));
 
-run();
+const mapped = data.map(calc),
+  part1 = mapped.reduce(sum, 0),
+  part2 = mapped.map(recursiveCalc).reduce(sum, 0);
+
+console.log("Part 1.1", part1); // 3291760
+console.log("Part 1.2", part2); // 4934767
